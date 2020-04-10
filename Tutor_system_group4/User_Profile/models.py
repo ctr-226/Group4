4154a6fcaaadd5e40f267c75e793bd26ecbb84c2
Shawhu-Liu <294737130@qq.com>
2020-04-11 00:46:32 +0800

    建立模型model

---
+++ Tutor_system_group4/User_Profile/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+# 导入内建的User模型。
+from django.contrib.auth.models import User
+# timezone 用于处理时间相关事务。
+from django.utils import timezone
+
+# 教师数据模型
+class Teacher(models.Model):
+    # 教师姓名 最长100字符
+    name = models.CharField(max_length=100)
+
+    # 教师年龄 限定正整数
+    age = models.PositiveIntegerField()
+
+    # 教师性别 chioce (u'内部存储名，u'外部显示名')
+    gender = ((u'M',u'男'),(u'F',u'女'),)
+
+    # 教师年级 chioce (u'内部存储名，u'外部显示名')
+    grade = (
+        (u'0',u'大一'),
+        (u'1',u'大二'),
+        (u'2',u'大三'),
+        (u'3',u'大四'),
+        (u'4',u'研一'),
+        (u'5',u'研二'),
+        (u'6',u'研三'),
+        (u'7',u'其他'),
+    )
+
+    # 邮箱
+    mailbox = models.EmailField(unique=True)
+
+    # 联系电话
+    phone = models.CharField(max_length=12,unique=True)
+
+# 学生数据模型
+class Student(models.Model):
+    # 学生姓名 最长100字符
+    name = models.CharField(max_length=100)
+
+    # 学生年龄 限定正整数
+    age = models.PositiveIntegerField()
+
+    # 学生性别 chioce (u'内部存储名，u'外部显示名')
+    gender = ((u'M',u'男'),(u'F',u'女'),)
+
+    # 学生年级 chioce (u'内部存储名，u'外部显示名')
+    grade = (
+        (u'0',u'小学一年级至三年级'),
+        (u'1',u'小学四年级至六年级'),
+        (u'2',u'初一'),
+        (u'3',u'初二'),
+        (u'4',u'初三'),
+        (u'5',u'高一'),
+        (u'6',u'高二'),
+        (u'7',u'高三'),
+        (u'8',u'其他'),
+    )
+
+    # 邮箱
+    mailbox = models.EmailField(unique=True)
+
+    # 联系电话
+    phone = models.CharField(max_length=12,unique=True)
---
+++ Tutor_system_group4/User_Profile/urls.py
@@ -0,0 +1,9 @@
+# 引入path
+from django.urls import path
+from . import views
+# 正在部署的应用的名称
+app_name = 'article'
+
+urlpatterns = [
+    #path('',view,namespace='')),
+]
