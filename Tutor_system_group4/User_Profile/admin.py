1f0f6d8b658e3661959db40ec9fdc2f289da7e62
CuBean <867564357@qq.com>
2020-04-15 14:40:47 +0800

    'rm cache'

--- Tutor_system_group4/User_Profile/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# 导入Teacher,Student
-from .models import Teacher,Student
-
-# 注册Teacher,Student到admin中
-admin.site.register(Teacher)
-admin.site.register(Student)
