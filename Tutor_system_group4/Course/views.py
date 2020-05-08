5ccbe047e54112417e2583f7d46058f3dac3cc78
tong <3568312831@qq.com>
2020-05-08 17:20:38 +0800

    修改年龄约束

--- Tutor_system_group4/Course/views.py
+++ Tutor_system_group4/Course/views.py
@@ -7,39 +7,43 @@ from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
-#课程申请匹配
+# 课程申请匹配
 def match(request):
     return None
 
-#增加课程
+
+# 增加课程
 def increase_course(request):
     return None
 
-#同意申请
+
+# 同意申请
 def agree_match(request):
     return None
 
-#课程详细内容
+
+# 课程详细内容
 
 def detail_course(request, id):
     course = CourseDetail.objects.get(id=id)
-    context = { 'course':course }
-    return render(request,'Course/detail.html',context )
+    context = {'course': course}
+    return render(request, 'Course/detail.html', context)
 
 
-#课程管理
+# 课程管理
 def manage_course(request, id):
     user = User.objects.get(id=id)
     context = {}
     if user.is_teacher:
-        # course_list = Teacher.objects.get(id=id).coursedetail_set.filter(state_match=True)
-        course_match = CourseDetail.objects.filter(teacher__id=id, state_match=True)
-        course_unmatched = CourseDetail.objects.filter(teacher__id=id, state_match=False)
+        course_list = Teacher.objects.get(id=id).coursedetail_set.filter(state_match=True)
+        #course_match = CourseDetail.objects.filter(teacher__id=id, state_match=True)
+        #course_unmatched = CourseDetail.objects.filter(teacher__id=id, state_match=False)
+        course_list = Teacher.objects.get(id=id).coursedetail_set.filter(state_match=True)
         context = {'course_match': course_match, 'course_unmatched': course_unmatched}
 
-        return render(request, '/Course/teacher_subject_detail.html', context)
+        return render(request, 'Course/teacher_subject_detail.html', context)
     if user.is_student:
         course_match = CourseDetail.objects.filter(student__id=id, state_match=True)
         course_unmatched = CourseDetail.objects.filter(student__id=id, state_match=False)
         context = {'course_match': course_match, 'course_unmatched': course_unmatched}
-        return render(request, '/Course/student_subject_detail.html', context)
+        return render(request, 'Course/student_subject_detail.html', context)
