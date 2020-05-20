84733de341c4d1652722e23fc7b7ffd8b792d3ec
Shauhu-Liu <294737130@qq.com>
2020-05-20 17:00:41 +0800

    用户头像完成

--- Tutor_system_group4/Tutor_system_group4/urls.py
+++ Tutor_system_group4/Tutor_system_group4/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from Course import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('User_Profile.urls'), name='user'),
@@ -26,3 +30,5 @@ urlpatterns = [
     path('index2/', views.index2, name='index2'),
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
