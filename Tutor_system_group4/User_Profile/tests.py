5f73118c2a4dfacb7b68dd0e59e8510753235a61
CuBean <867564357@qq.com>
2020-05-27 11:52:30 +0800

    '一点注释'

--- Tutor_system_group4/User_Profile/tests.py
+++ Tutor_system_group4/User_Profile/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client, LiveServerTestCase
+from .models import Teacher, Student, User, AbstractUser
+from django.urls import reverse
+
+
+class LoginViewTests(TestCase):
+
+    def setUp(self):
+        Teacher.objects.create(id=1, name='teacher1', age=20, gender='M', grade='1', mailbox='111@qq.com')
+        Student.objects.create(id=1, name='teacher1', age=15, gender='F', grade='5', mailbox='111@qq.com')
+
+    def test_login_with_right_user(self):
+        """"""
 
 # Create your tests here.
