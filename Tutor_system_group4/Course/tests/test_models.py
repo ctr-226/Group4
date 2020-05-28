900aaea12a8d7339999e9942bab6bbe6940272a9
Shauhu-Liu <294737130@qq.com>
2020-05-29 00:23:48 +0800

    test.py

--- Tutor_system_group4/Course/tests/test_models.py
+++ Tutor_system_group4/Course/tests/test_models.py
@@ -7,23 +7,30 @@ from django.shortcuts import render, redirect
 
 from Course.forms import CourseForm
 from Course.models import CourseDetail
-
+from django.utils import timezone
 
 class CourseModelTest(TestCase):
-
+    # 测试基本的模型属性
     @classmethod
-    def setUpTestCourseData(cls):
-        new_user = User.objects.create_user(username='test_user_teacher', password='password_test', is_teacher=True)
-        Teacher.objects.create(teacher_user=new_user, name='test_user_teacher', mailbox="mail@gmail.com",
-                               phone='18238099856')
-        teacher = Teacher.objects.get(id=1)
-        CourseDetail.objects.create(teacehr=teacher,nick_name='teat_class',grade_course='8',subject='zh')
-    
+    def setUp(self):
+        # 设置好两个学生账户和一个老师账户
+        new_user = User.objects.create_user(username='test_user_student', password='password_test1', is_student=True)
+        student1 = Student.objects.create(student_user=new_user, mailbox="mail@gmail.com", phone='18238099856')
+        new_user1 = User.objects.create_user(username='test_user_student1', password='password_test1', is_student=True)
+        student2 = Student.objects.create(student_user=new_user1, mailbox="mail@gmail.com", phone='18238099856')
+        new_user2 = User.objects.create_user(username='test_user_teacher', password='password_test2', is_teacher=True)
+        teacher1 = Teacher.objects.create(teacher_user=new_user2, name='test_user_teacher', mailbox="mail@gmail.com",
+                                          phone='18238099856')
+        course = CourseDetail.objects.create(teacher=teacher1, nick_name='class1', grade_course='1', subject='zh',
+                                             introduction='hello', charge=1, time_set=timezone.now(), state_match=0)
+        course.student_applied.add(student1, student2)
+
+
     def test_nick_name_label(self):
         course = CourseDetail.objects.get(id=1)
         label = course._meta.get_field('nick_name').verbose_name
         self.assertEquals(label,'课程名称')
-
+    
     def test_nick_name_length(self):
         course = CourseDetail.objects.get(id=1)
         label = course._meta.get_field('nick_name').max_length
